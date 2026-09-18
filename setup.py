74b85862751311c951e0915a5c65e1666baccfd3
Tapasya Chaturvedi <chaturveditapasya2005@gmail.com>
2026-09-18 21:10:54 +0530

    project setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Tapasya",
+    author_email="tapasyachaturvediaug@gmail.com",
+    packages=find_packages()
+)
